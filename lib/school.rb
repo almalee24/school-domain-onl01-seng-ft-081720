b5909a0c2c4c378e79218925f3affe536297f1d7
Alma Flores <flores.almalee@gmail.com>
2020-08-24 22:55:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,12 @@
 class School 
-  def initialize(school_namename)
+  def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
   
-  def add_student(name, grade)
+  def add_student(student_name, grade)
     if @roster[grade]
-      
+      @roster[grade] << student_name
+    else 
+      @roster[grade] = []
 end 
